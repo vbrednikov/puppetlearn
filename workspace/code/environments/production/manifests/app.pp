32367a8ba5cce6c45c3680f8a0fa0d3172b47c71
Vladimir Brednikov <vbrednikov@gmail.com>
2018-03-04 00:05:18 +0300

    add checkpoint and default node definition

--- workspace/code/environments/production/manifests/app.pp
+++ workspace/code/environments/production/manifests/app.pp
@@ -1,5 +1,16 @@
 node 'node1' {
 
+    notify {"***** NODE1 BLOCK *****": }
     include java::install
+
+}
+
+node default {
+
+    notify{"checkpoint_1":
+
+#       message => "***** DEFAULT BLOCK *****"
+
+    }
 
 }
