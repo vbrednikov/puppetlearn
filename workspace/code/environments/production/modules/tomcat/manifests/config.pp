2a293c8ababfa7b9bac2315a4e184961f80deafa
Vladimir Brednikov <vbrednikov@gmail.com>
2018-03-21 00:25:42 +0300

    total parametrization of tomcat.conf

--- workspace/code/environments/production/modules/tomcat/manifests/config.pp
+++ workspace/code/environments/production/modules/tomcat/manifests/config.pp
@@ -2,7 +2,7 @@ class tomcat::config inherits tomcat {
 
     file { $::tomcat::config_path:
         ensure => file,
-        source => 'puppet:///modules/tomcat/tomcat.conf',
+        content => template('tomcat/tomcat.conf.erb'),
         mode   => '0644',
         owner  => $::tomcat::user,
         group  => $::tomcat::group,
--- workspace/code/environments/production/modules/tomcat/manifests/params.pp
+++ workspace/code/environments/production/modules/tomcat/manifests/params.pp
@@ -7,4 +7,19 @@ class tomcat::params {
     $service_name   = 'tomcat'
     $service_state  = running
 
+    $shutdown_wait     = 30
+    $catalina_pid      = '/var/run/tomcat.pid'
+    $tomcat_cfg_loaded = 1
+    $java_home         = '/usr/lib/jvm/jre'
+    $xmx               = '128m'
+    $xms               = '68m'
+    $max_perm_size     = '128m'
+    $catalina_base     = '/usr/share/tomcat'
+    $catalina_home     = '/usr/share/tomcat'
+    $jasper_home       = '/usr/share/tomcat'
+    $catalina_tmpdir   = '/usr/share/tomcat'
+    $security_manager  = 'false'
+    $shutdown_verbose  = 'false'
+    $connector_port    = "8080"
+
 }
